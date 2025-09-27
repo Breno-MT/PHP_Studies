3b704f902bab8236188add13454e9c5d69d770c2
Breno-MT <brenomtdev@gmail.com>
2025-09-27 13:51:03 -0300

    FEAT: type of data (missing a bunch but you get the drift)

---
+++ Data_Types/data_type.php
@@ -0,0 +1,12 @@
+<?php
+// Use var_dump to get the type of the data that is being casted.
+
+$x = 4;
+$y = 5;
+
+var_dump($x);
+var_dump($y);
+
+$cars = array("BMW", "Volvo", "Audio");
+var_dump($cars);
+?>
